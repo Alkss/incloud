e8cfec96541af854083d3d8537556dc00d6071f5
Alkss <alkss_fjv@yahoo.com.br>
2019-04-21 13:10:28 -0300

    Project update

---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="shortcut icon" href="/imgs/favicon.ico"/>
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/bootstrap/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="/style/custom.css" rel="stylesheet">
+    <script src="/bootstrap/js/jquery-3.3.1.js"></script>
+    <script src="/bootstrap/js/dataTables.bootstrap.min.js"></script>
+    <script src="/bootstrap/js/jquery.dataTables.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container main">
